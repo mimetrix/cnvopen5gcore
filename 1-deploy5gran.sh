dc9526fccb85ad7f38739c6bc4d70d2927cf1f13
Thiago do Amaral <t.amaral@f5.com>
2025-10-29 20:11:03 -0300

    fix: remove quotes from sed command in gnb config

--- 1-deploy5gran.sh
+++ 1-deploy5gran.sh
@@ -17,7 +17,7 @@ oc get services -n open5gcore | grep amf-open5gs-sctp | awk '{print $3}' > amf-i
 echo "AMF IP:" && cat amf-ip
 cp templates/5gran-gnb-configmap.bak templates/5gran-gnb-configmap.yaml
 cp templates/5gran-ue-configmap.bak templates/5gran-ue-configmap.yaml
-AMF_IP=$(cat amf-ip) && sed -i '' "s/<put-your-amf-service-ip-here>/${AMF_IP}/g" templates/5gran-gnb-configmap.yaml
+AMF_IP=$(cat amf-ip) && sed -i "s/<put-your-amf-service-ip-here>/${AMF_IP}/g" templates/5gran-gnb-configmap.yaml
 echo "gNB Config:" && cat templates/5gran-gnb-configmap.yaml
 helm install -f values.yaml 5gran ./
 echo "Enjoy The 5GRAN!"
