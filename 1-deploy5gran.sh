7a557abda6c808c8797f8bafad66ba4bc7f56946
Thiago do Amaral <t.amaral@f5.com>
2025-10-29 17:46:46 -0300

    fix: make it to work in internal cluster

--- 1-deploy5gran.sh
+++ 1-deploy5gran.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 #Author: fenar
-oc new-project open5gran
+
+# for some reason the communication doesn't work if deployed in a different namespace.
+# Need to investigate further.
+# oc new-project open5gran
+
 oc adm policy add-scc-to-user anyuid -z default -n open5gran
 oc adm policy add-scc-to-user hostaccess -z default -n open5gran
 oc adm policy add-scc-to-user hostmount-anyuid -z default -n open5gran
@@ -13,7 +17,7 @@ oc get services -n open5gcore | grep amf-open5gs-sctp | awk '{print $3}' > amf-i
 echo "AMF IP:" && cat amf-ip
 cp templates/5gran-gnb-configmap.bak templates/5gran-gnb-configmap.yaml
 cp templates/5gran-ue-configmap.bak templates/5gran-ue-configmap.yaml
-sed -e "s/<put-your-amf-service-ip-here>/$(<amf-ip sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' | tr -d '\n')/g" -i templates/5gran-gnb-configmap.yaml
+AMF_IP=$(cat amf-ip) && sed -i '' "s/<put-your-amf-service-ip-here>/${AMF_IP}/g" templates/5gran-gnb-configmap.yaml
 echo "gNB Config:" && cat templates/5gran-gnb-configmap.yaml
 helm install -f values.yaml 5gran ./
 echo "Enjoy The 5GRAN!"
